cf7c37b95f1f6f50897194f2eb00c7273cc3a6fe
Pharphy-13 <148485703+Pharphy-13@users.noreply.github.com>
2024-08-27 07:58:18 +0200

    Add files via upload

---
+++ banking/src/Bank.java
@@ -0,0 +1,34 @@
+public class Bank {
+    private final String accountType;
+    private double accountBalance;
+
+    // Parameterized constructor
+    public Bank(String accountType, double accountBalance) {
+        this.accountType = accountType;
+        this.accountBalance = accountBalance;
+    }
+
+    // Method to deposit money
+    public double deposit(double amount) {
+        accountBalance += amount;
+        return accountBalance;
+    }
+
+    // Method to withdraw money
+    public double withdrawal(double amount) {
+        if (amount <= accountBalance) {
+            accountBalance -= amount;
+            double newBalance = withdrawal(100.0);
+            System.out.println("New balance: " + newBalance);
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+        return accountBalance;
+
+    }
+
+    // Method to display account information
+    public void display() {
+        System.out.println("The account type is " + accountType + " and the balance is " + accountBalance);
+    }
+}
---
+++ banking/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating an instance of the Bank class
+        Bank myBankAccount = new Bank("SD", 20041331);
+        myBankAccount.display(); // Invoking the display method
+
+    }
+}
